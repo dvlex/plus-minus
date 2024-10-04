b02d500e3b42699dcf2fd8e9076ec71c3d56c3c1
Alex Castillo <alex@lexdrel.com>
2024-10-04 00:52:34 +0000

    refactor and clean code

--- Program.cs
+++ Program.cs
@@ -1,23 +1,6 @@
-﻿
-using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
-using System;
-
-// See https://aka.ms/new-console-template for more information
-List<int> arr = new List<int> { -4, 3, -9, 0, 4, 1 };
+﻿List<int> arr = new List<int> { -4, 3, -9, 0, 4, 1 };
 Result.plusMinus(arr);
 
-Console.WriteLine("Hello, world!");
 class Result
 {
     public static void plusMinus(List<int> arr)
